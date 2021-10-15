9e3c998101b2312f61a8c121b6df4b6e49f87e8b
shamimbinnur <sbnsbn00@gmail.com>
2021-10-15 20:33:28 +0600

    final

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./contact.scss";
+
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/inbox.png" alt="" />
+      </div>
+      <div className="right">
+        <h2>Get in touch.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,27 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            David.
+          </a>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>david@mail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
